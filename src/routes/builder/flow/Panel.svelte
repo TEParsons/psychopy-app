8c6c68ba06a0879344a6bc34246ab77c11350441
Todd OST <todd.e.parsons@gmail.com>
2025-03-17 13:38:41 +0000

    ENH: Add flow buttons

--- src/routes/builder/flow/Panel.svelte
+++ src/routes/builder/flow/Panel.svelte
@@ -5,16 +5,27 @@
 <div class=flow-panel>
     <div class=flow-buttons>
         <button class=horizontal id=add-routine>
-            <img src="" alt="" />
+            <img src="/icons/light/btn-loop.svg" alt="" />
             <label for=add-routine>Add Routine</label>
         </button>
-        <button>Add Loop</button>
+        <button class=horizontal id=add-loop>
+            <img src="/icons/light/btn-routine.svg" alt="" />
+            <label for=add-loop>Add Loop</label>
+        </button>
     </div>
     <div class=flow-canvas>
-
+        
     </div>
 </div>
 
 <style>
-
+    .flow-panel {
+        display: grid;
+        grid-template-columns: [buttons] min-content ;
+    }
+    .flow-buttons {
+        padding: 1rem;
+        display: grid;
+        grid-auto-flow: row;
+    }
 </style>
