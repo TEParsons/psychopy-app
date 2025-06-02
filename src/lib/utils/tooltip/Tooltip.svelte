2a61bea5df04324f1beaabc76f967e167968d594
Todd OST <todd.e.parsons@gmail.com>
2025-06-02 09:44:49 +0100

    ENH: Add slight delay to tooltip

--- src/lib/utils/tooltip/Tooltip.svelte
+++ src/lib/utils/tooltip/Tooltip.svelte
@@ -1,4 +1,13 @@
-<div class=tooltip>
+<script>
+    /** @prop @type {number} Delay before showing this tooltip */
+    export let delay = 0.5;
+</script>
+
+
+<div 
+    class=tooltip
+    style="--tooltip-delay: {delay}s"
+>
     <slot></slot>
 </div>
 
@@ -17,10 +26,12 @@
         font-weight: normal;
         /* hidden until hovered */
         visibility: hidden;
+        transition: visibility linear 0s 0s;
     }
 
     :global(*:hover) > .tooltip,
     :global(*:focus) > .tooltip {
+        transition: visibility linear 0s var(--tooltip-delay);
         visibility: visible;
     }
 </style>
