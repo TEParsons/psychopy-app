6e5574d540d93f5a3b601de835c983185c2310dc
Todd OST <todd.e.parsons@gmail.com>
2025-06-20 11:58:34 +0100

    SYS: Add a scratch panel for myself on the homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,29 +10,71 @@
     function new_runner_frame() {
         window.open("/runner");
     }
+
+    import { writable } from "svelte/store";
+    let scratchOutput = writable("")
+    function scratch(evt) {
+        let output = "";
+
+        scratchOutput.set(String(output))
+    }
 </script>
 
 <div class=container>
-    <button class=horizontal id="ribbon-btn-builder" onclick={new_builder_frame}>
-        <img src="/icons/{$theme}/btn-builder.svg" alt="Builder view" />
-        <label for="ribbon-btn-builder">Builder</label>
-    </button>
-    <button class=horizontal id="ribbon-btn-coder" onclick={new_coder_frame}>
-        <img src="/icons/{$theme}/btn-coder.svg" alt="Builder view" />
-        <label for="ribbon-btn-coder">Coder</label>
-    </button>
-    <button class=horizontal id="ribbon-btn-runner" onclick={new_runner_frame}>
-        <img src="/icons/{$theme}/btn-runner.svg" alt="Builder view" />
-        <label for="ribbon-btn-runner">Runner</label>
-    </button>
+    <nav>
+        <button class=horizontal id="ribbon-btn-builder" on:click={new_builder_frame}>
+            <img src="/icons/{$theme}/btn-builder.svg" alt="Builder view" />
+            <label for="ribbon-btn-builder">Builder</label>
+        </button>
+        <button class=horizontal id="ribbon-btn-coder" on:click={new_coder_frame}>
+            <img src="/icons/{$theme}/btn-coder.svg" alt="Builder view" />
+            <label for="ribbon-btn-coder">Coder</label>
+        </button>
+        <button class=horizontal id="ribbon-btn-runner" on:click={new_runner_frame}>
+            <img src="/icons/{$theme}/btn-runner.svg" alt="Builder view" />
+            <label for="ribbon-btn-runner">Runner</label>
+        </button>
+    </nav>
+    <div class=scratch>
+        <textarea
+            class=scratch-output
+            readonly
+            bind:value={$scratchOutput}
+        ></textarea>
+        <button
+            class=scratch-btn
+            on:click={scratch}
+        >Scratch</button>
+    </div>
 </div>
 
 <style>
 .container {
-    display: grid;
-    grid-auto-flow: row;
+    position: relative;
+    width: 100%;
+    height: 100%;
+}
+nav {
+    position: relative;
+    display: flex;
+    flex-direction: row;
     margin: 4rem auto;
-    grid-gap: 2rem;
-    width: 20rem;
+    gap: 2rem;
+    justify-content: center;
+}
+nav button {
+    position: relative;
+    width: 8rem;
+}
+.scratch {
+    position: relative;
+    width: 1000px;
+    margin: auto;
+}
+.scratch * {
+    width: 100%;
+}
+.scratch-output {
+    height: 600px;
 }
 </style>
