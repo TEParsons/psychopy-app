a63a975b3a62d73de32afc630ee28d50f97cf236
Todd OST <todd.e.parsons@gmail.com>
2025-07-14 12:37:47 +0100

    ENH: Add hover labels to ribbon buttons

--- src/lib/utils/ribbon/Button.svelte
+++ src/lib/utils/ribbon/Button.svelte
@@ -1,23 +1,42 @@
 <script>
+    import { slide } from 'svelte/transition';
+
     let {
         /** @prop @type {string} Text label for this button, if any */
         label="",
-        /** @prop @type {string} Icon for this section, if any */
+        /** @prop @type {string} Icon for this button, if any */
         icon=undefined,
         /** @prop @type {function} Function to call when this button is clicked */
-        onclick=() => {},
+        onclick=(evt) => {},
         /** @prop @type {boolean} Disable this button */
         disabled=false
     } = $props()
 
-</script>
+    let showLabel = $state(false)
+</script>       
 
-<button 
+<button
     disabled={disabled} 
     onclick={onclick}
+    onmouseenter={() => {showLabel = true}}
+    onmouseleave={() => {showLabel = false}}
+    onfocusin={() => {showLabel = true}}
+    onfocusout={() => {showLabel = false}}
 >
     <img src={icon} alt={label} />
+    {#if showLabel}
+        <span 
+            class=btn-label
+            transition:slide={{axis: 1}}
+        >
+            {label}
+        </span>
+    {/if}
 </button>
+
+
+
+
 
 <style>
     button {
@@ -28,8 +47,12 @@
         grid-row-start: buttons;
         outline: none;
         border: 1px solid transparent;
-        transition: border-color .2s, box-shadow .2s;
         border-radius: .5rem;
+        transition: border-color .2s, box-shadow .2s;
+        display: grid;
+        grid-auto-flow: column;
+        align-items: center;
+        gap: .2rem;
     }
     button:disabled {
         opacity: .5;
@@ -48,4 +71,9 @@
     button img {
         height: 2.25rem;
     }
+    .btn-label {
+        padding-right: .5rem;
+        text-wrap: nowrap;
+    }
+
 </style>
