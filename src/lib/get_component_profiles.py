5506cbd9632bbdf88895d2746ef249846d6aee12
TEParsons <todd.e.parsons@gmail.com>
2025-08-13 16:11:25 +0100

    ENH: Update script to get component JSON from PsychoPy

--- src/lib/get_component_profiles.py
+++ src/lib/get_component_profiles.py
@@ -1,8 +1,10 @@
 from psychopy.experiment import getAllElements, loops, devices
+from psychopy.plugins import activatePlugins
 
 from pathlib import Path
 import json
 
+activatePlugins()
 
 # custom method to handle non-serializable values
 def default(o):
@@ -15,7 +17,7 @@ def default(o):
 profiles = {}
 # append JSON of each element
 for name, cls in getAllElements().items():
-    profiles[name] = cls.getJSON()
+    profiles[name] = cls.getTemplateJSON()
 
 # save to json
 with (Path(__file__).parent / "components.json").open("w") as f:
@@ -27,7 +29,7 @@ with (Path(__file__).parent / "components.json").open("w") as f:
 profiles = {}
 # append JSON of each loop type
 for cls in loops._BaseLoopHandler.__subclasses__():
-    profiles[cls.__name__] = cls.getJSON()
+    profiles[cls.__name__] = cls.getTemplateJSON()
 # save to json
 with (Path(__file__).parent / "loops.json").open("w") as f:
     json.dump(
@@ -38,7 +40,7 @@ with (Path(__file__).parent / "loops.json").open("w") as f:
 profiles = {}
 # append JSON of each loop type
 for cls in devices.DeviceBackend.__subclasses__():
-    profiles[cls.__name__] = cls.getJSON()
+    profiles[cls.__name__] = cls.getTemplateJSON()
     profiles[cls.__name__]['params']['name'] = profiles[cls.__name__]['params'].pop('deviceLabel')
 # save to json
 with (Path(__file__).parent / "devices.json").open("w") as f:
