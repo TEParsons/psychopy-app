3391be98f906083100ca9c2a2cc5b633c2168571
Todd OST <todd.e.parsons@gmail.com>
2025-10-23 10:48:00 +0100

    NF: Functionality to open Excel spreadsheet from param ctrl

--- src/lib/paramCtrls/ctrls/TableCtrl.svelte
+++ src/lib/paramCtrls/ctrls/TableCtrl.svelte
@@ -1,6 +1,8 @@
 <script>
+    import { getContext } from "svelte";
     import { CompactButton } from "$lib/utils/buttons";
     import FileCtrl from "./FileCtrl.svelte";
+    import { electron, python } from "$lib/globals.svelte"
 
     let {
         /** @prop @type {import("$lib/experiment/experiment.svelte.js").Param} Param object to which this ctrl pertains */
@@ -11,10 +13,30 @@
         ...attachments
     } = $props()
 
+    let current = getContext("current")
+
     function validateTable(valid) {}
 
     function openTable() {
-        // placeholder: How do I open Excel from Svelte? 
+        electron.files.openPath(current.experiment.relativePath(param.val))
+    }
+
+    function newTable() {
+        if (param?.ctrlParams?.template) {
+            // if a table is given, open it
+            electron.files.openPath(param.ctrlParams.template)
+        } else {
+            // otherwise, get the default from Python...
+            python.liaison.send({
+                command: "run",
+                args: [
+                    "psychopy.experiment.utils:getBlankTemplate"
+                ]
+            }, 1000).then(
+                // ...and open it
+                resp => electron.files.openPath(resp)
+            )
+        }
     }
 
 </script>
@@ -26,13 +48,11 @@
     {@attach element => param.registerValidator("table", validateTable, -5)}
     {...attachments}
 ></FileCtrl>
-<CompactButton
-    icon="/icons/btn-table.svg"
-    tooltip="{param.val ? "Open" : "Create"} table"
-    onclick={openTable}
-    disabled={disabled || (
-        param.val && (
-            String(param.val).startsWith("$")
-        )
-    )}
-></CompactButton>
+{#if electron}
+    <CompactButton
+        icon="/icons/btn-{param.val ? "" : "new-"}table.svg"
+        tooltip="{param.val ? "Open" : "Create"} table"
+        onclick={param.val ? openTable : newTable}
+        disabled={disabled || param.isCode}
+    />
+{/if}
