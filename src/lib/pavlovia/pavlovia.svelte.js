be3972cde0cce06363356e81b22f5ccea9f5960f
Todd OST <todd.e.parsons@gmail.com>
2025-11-21 11:18:33 +0000

    ENH: Simplify pavlovia login

--- src/lib/pavlovia/pavlovia.svelte.js
+++ src/lib/pavlovia/pavlovia.svelte.js
@@ -37,8 +37,7 @@ export var auth = $state({
     }
 })
 
-
-export async function getAuthURL() {
+export async function login() {
     // create a private "state" based on uuid
     auth.state = String(crypto.randomUUID())
     // first make the answer - pick random alphanumeric chars
@@ -50,56 +49,19 @@ export async function getAuthURL() {
     let hash = await crypto.subtle.digest("SHA-256", new TextEncoder().encode(auth.verifier))
     // decode hash to make challenge
     auth.challenge = btoa(String.fromCharCode(...new Uint8Array(hash))).replace("=", "")
-    
-    // construct auth url
-    return (
-        `${auth.root}/oauth/authorize?`
-        + `client_id=${auth.client}`
-        + `&redirect_uri=${auth.redirect}`
-        + `&response_type=code`
-        + `&state=${auth.state}`
-        + `&code_challenge=${auth.challenge}`
-        + `&code_challenge_method=S256`
-    )
-}
-
-export async function login() {
-    // open authentication url in a new tab
-    let url = await getAuthURL();
-    let win = window.open(url);
-    // request the "code" param from that tab when ready
-    let codeResp = win.postMessage({
-        method: "POST",
-        body: JSON.stringify({
-            'client_id': auth.client, 
-            'code': undefined,
-            'grant_type': "authorization_code",
-            'redirect_uri': auth.redirect,
-            'code_verifier': auth.verifier
-        }),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        }
+    // construct auth url params
+    let params = new URLSearchParams({
+        client_id: auth.client,
+        redirect_uri: window.location.href,
+        response_type: "code",
+        state: auth.state,
+        code_challenge: auth.challenge,
+        code_challenge_method: "S256"
     })
-
-    return
-    // use that code to request an OAuth from pavlovia
-    let authResp = await fetch("https://gitlab.pavlovia.org/oauth/token", {
-        method: "POST",
-        body: JSON.stringify({
-            'client_id': auth.client,
-            'code': codeResp.json()['code'],
-            'grant_type': "authorization_code",
-            'redirect_uri': auth.redirect,
-            'code_verifier': auth.verifier
-        }),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        }
-    });
-    // store OAuth
-    auth.token.type = authResp.json()['token_type']
-    auth.token.token = authResp.json()['token']
+    // open authentication url
+    window.open(
+        `${auth.root}/oauth/authorize?${params.toString()}`
+    );
 }
 
 export function logout() {
